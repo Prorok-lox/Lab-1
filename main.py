56007f5085c51906914460fc45bcdb11471596b6
ILYA <il_vlad@inbox.ru>
2023-10-22 02:46:25 +0300

    добавлены решения на все задания

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# RED = '\u001b[41m'
-# BLUE = '\u001b[44m'
-# WHITE = '\u001b[47m'
-# BLACK = '\u001b[30m'
-# END = '\u001b[0m'
+RED = '\u001b[41m'
+BLUE = '\u001b[44m'
+WHITE = '\u001b[47m'
+BLACK = '\u001b[30m'
+END = '\u001b[0m'
 
-# # for i in range(6):
-# #     if i < 2:
-# #         print(f'{RED}{"  " * 14}{END}')
-# #     elif i > 1 and i < 4:
-# #         print(f'{WHITE}{"  " * 14}{END}')
-# #     else:
-# #         print(f'{BLUE}{"  " * 14}{END}')
+# for i in range(6):
+#     if i < 2:
+#         print(f'{RED}{"  " * 14}{END}')
+#     elif i > 1 and i < 4:
+#         print(f'{WHITE}{"  " * 14}{END}')
+#     else:
+#         print(f'{BLUE}{"  " * 14}{END}')
 
 
 # plot_list = [[0 for i in range(10)] for i in range(10)]
@@ -20,7 +20,6 @@
 #     result[i] = 2 * i
 
 # step = round(abs(result[0] - result[9]) / 9, 2)
-# print(step)
 
 # for i in range(10):
 #     for j in range(10):
@@ -74,7 +73,6 @@
 # print(round(sum(nechet),2), round(sum(chet),2))
 
 # size = 20
-
 # for i in range(size):
 #     for j in range(size):
 #         if i == j or i + j == size - 1:
@@ -82,3 +80,84 @@
 #         else:
 #             print(" ", end="")
 #     print()
+
+
+# WHITE = '\u001b[47m'
+# BLACK = '\u001b[97m'
+# RESET = '\u001b[0m'  # Для сброса цветов после окончания крестика
+
+# def draw_two_black_crosses_45_degrees(size):
+#     for i in range(size):  # Итерируемся по строкам (горизонталь)
+#         # Рисуем первый крестик (левый) с черными символами на белом фоне
+#         for j in range(size):  # Итерируемся по столбцам (вертикаль)
+#             if i == j or i + j == size - 1:  # Условие для рисования крестика
+#                 print(f"{WHITE}  {RESET}", end='')  # Выводим два пробела с белым фоном
+#             else:
+#                 print(f"{BLACK}  {RESET}", end='')  # Выводим два пробела с черными символами на белом фоне
+#         # Рисуем второй крестик (правый) с черными символами на белом фоне
+#         for j in range(1, size):  # Итерируемся по столбцам, начиная с индекса 1
+#             if i == j or i + j == size - 1:  # Условие для рисования крестика
+#                 print(f"{WHITE}  {RESET}", end='')  # Выводим два пробела с белым фоном
+#             else:
+#                 print(f"{BLACK}  {RESET}", end='')  # Выводим два пробела с черными символами на белом фоне
+#         print()  # Переходим на следующую строку после завершения рисования обоих крестиков
+
+# # Пример использования:
+# draw_two_black_crosses_45_degrees(11)  # Вызываем функцию с размером 11 для вывода узора
+
+
+
+
+# def draw_dutch_flag():
+#     # Определение цветов
+#     RED = '\u001b[41m'
+#     WHITE = '\u001b[47m'
+#     BLUE = '\u001b[44m'
+#     RESET = '\u001b[0m'
+
+#     # Размер флага
+#     width = 18  # Ширина флага
+#     height = 6  # Высота флага
+
+#     # Рисуем флаг Нидерландов
+#     for i in range(height):
+#         if i < height // 3:
+#             color = RED
+#         elif i < 2 * height // 3:
+#             color = WHITE
+#         else:
+#             color = BLUE
+
+#         for j in range(width):
+#             print(f"{color}  {RESET}", end='')
+
+#         print()
+
+# # Пример использования:
+# draw_dutch_flag()
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# # Создаем массив значений x от -10 до 10 с шагом 0.1
+# x = np.arange(-10, 10, 0.1)
+
+# # Вычисляем соответствующие значения y = 2x
+# y = 2 * x
+
+# # Строим график
+# plt.plot(x, y, label='y = 2x')
+
+# # Добавляем заголовок и метки осей
+# plt.title('График функции y = 2x')
+# plt.xlabel('x')
+# plt.ylabel('y')
+
+# # Включаем сетку
+# plt.grid(True)
+
+# # Добавляем легенду
+# plt.legend()
+
+# # Отображаем график
+# plt.show()
